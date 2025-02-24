3227b3eec7d276d9cd79efc9a7c10d147b4b112c
gogigogigogi <136942815+gogigogigogi@users.noreply.github.com>
2025-02-24 14:08:52 +0900

    [Refactor]gameSetup 리팩토링

--- socket/game/gameSetup.js
+++ socket/game/gameSetup.js
@@ -21,38 +21,29 @@ const {
 
 exports.readyGameHandler = async (io, socket) => {
   try {
-    const { userId, nickname, gameId } = getPlayerFromUsersInfo(socket.id);
-    const gameInfo = getGameInfoByGameId(gameId);
-    const {
-      name,
-      currentTurnId,
-      currentRound,
-      maxRound,
-      isLock,
-      pw,
-      manager,
-      isWaiting,
-      players,
-    } = gameInfo;
+    const userInfo = getPlayerFromUsersInfo(socket.id);
+    const gameInfo = getGameInfoByGameId(userInfo.gameId);
+
     // 참가 가능 방 여부 확인
     if (!gameInfo) throw new Error("존재하지 않는 방입니다.");
-    if (!isWaiting) throw new Error("시작된 방입니다.");
+    if (!gameInfo.isWaiting) throw new Error("시작된 방입니다.");
     // 내가 참가중인 방인지 확인
-    const isEntering = isUserInGame(gameId, userId);
+    const isEntering = isUserInGame(userInfo.gameId, userInfo.userId);
     if (!isEntering) throw new Error("참가 중인 방이 아닙니다.");
     // 방장 여부 확인
-    if (manager === userId) throw new Error("방장은 준비상태를 변경할 수 없습니다.");
+    if (gameInfo.manager === userInfo.userId)
+      throw new Error("방장은 준비상태를 변경할 수 없습니다.");
 
     // 레디 상태 토글
-    toggleReadyGamesInfo(gameId, userId);
+    toggleReadyGamesInfo(userInfo.gameId, userInfo.userId);
 
     // readyGame 성공 응답객체
     const readyGameRes = getReadyRes(socket.id, "준비 성공");
     // updateParticipants 성공 응답객체
     const updateGameInfoRes = getUpdateGameInfoRes(socket.id);
 
-    io.of("/game").to(gameId).emit("readyGame", readyGameRes);
-    io.of("/game").to(gameId).emit("updateGameInfo", updateGameInfoRes);
+    io.of("/game").to(userInfo.gameId).emit("readyGame", readyGameRes);
+    io.of("/game").to(userInfo.gameId).emit("updateGameInfo", updateGameInfoRes);
   } catch (err) {
     console.log(err);
     const readyErrRes = getErrorRes(socket.id, err.message);
@@ -61,47 +52,37 @@ exports.readyGameHandler = async (io, socket) => {
 };
 
 exports.startGameHandler = async (io, socket) => {
-  const { userId, nickname, gameId } = getPlayerFromUsersInfo(socket.id);
-  const gameInfo = getGameInfoByGameId(gameId);
-  console.log("start시 게임정보", gameInfo);
-  const {
-    name,
-    currentTurnUserId,
-    currentRound,
-    maxRound,
-    isLock,
-    pw,
-    manager,
-    isWaiting,
-    players,
-  } = gameInfo;
-
   try {
+    const userInfo = getPlayerFromUsersInfo(socket.id);
+    const gameInfo = getGameInfoByGameId(userInfo.gameId);
+
+    console.log("start시 게임정보", gameInfo);
     // 참가 가능 방 여부 확인
     if (!gameInfo) throw new Error("존재하지 않는 방입니다.");
     if (!gameInfo.isWaiting) throw new Error("시작된 방입니다.");
     // 내가 참가중인 방인지 확인
-    const isEntering = isUserInGame(gameId, userId);
+    const isEntering = isUserInGame(userInfo.gameId, userInfo.userId);
     if (!isEntering) throw new Error("참가 중인 방이 아닙니다.");
     // 방장 여부 확인
-    if (manager !== userId) throw new Error("방장만 시작할 수 있습니다.");
+    if (gameInfo.manager !== userInfo.userId)
+      throw new Error("방장만 시작할 수 있습니다.");
     // 2인 이상 시작
-    if (players.length < 2)
+    if (gameInfo.players.length < 2)
       throw new Error("유저가 2명 이상 있을때만 시작할 수 있습니다.");
 
     // 전체 유저의 준비상태를 체크
-    const isAllReady = checkAllReady(gameId);
+    const isAllReady = checkAllReady(userInfo.gameId);
     if (!isAllReady) {
       throw new Error("전체 유저가 Ready 상태가 아닙니다.");
     }
 
     // db 정보 변경
-    const updateResult = await updateWaitingStatus(gameId);
+    const updateResult = await updateWaitingStatus(userInfo.gameId);
     if (!updateResult[0]) throw new Error("isWaiting 상태를 변경할 수 없습니다.");
 
     // 스타트를 누르면 게임을 하기위한 세팅값 설정
-    const newCurrentTurnUserId = players[0].userId;
-    const newMaxRound = players.length * maxRound;
+    const newCurrentTurnUserId = gameInfo.players[0].userId;
+    const newMaxRound = gameInfo.players.length * gameInfo.maxRound;
     const newCurrentRound = 1;
     const newIsWaiting = false;
     const newIsAnswerFound = false;
@@ -111,7 +92,7 @@ exports.startGameHandler = async (io, socket) => {
     const newIsGameEnd = false;
 
     setGameFromGamesInfo({
-      gameId,
+      gameId: userInfo.gameId,
       newCurrentTurnUserId,
       newMaxRound,
       newCurrentRound,
@@ -123,7 +104,7 @@ exports.startGameHandler = async (io, socket) => {
       newIsGameEnd,
     });
 
-    console.log("게임시작시 게임정보", socketGamesInfo[gameId]);
+    console.log("게임시작시 게임정보", socketGamesInfo[userInfo.gameId]);
     emitRoundStartWithTurn(io, socket.id, "startGame");
   } catch (err) {
     console.log(err);
